16d2853a188b94c7d99e98a6026cecf634f122d4
Adam Selker <aselker@openmailbox.org>
2017-10-10 12:12:14 -0400

    First pass of PID control

--- testPID/src/main.cpp
+++ testPID/src/main.cpp
@@ -3,13 +3,18 @@
 #include "Arduino.h"
 #include <Adafruit_MotorShield.h>
 
+const int readDelay = 10;
+const float sensorDiffToLinePos = 0.1;
+const float kp = 1, ki = 0.001, kd = 100;
+
+const int numLeds = 3;
 const int ledPins = {4, 5, 6};
 const int sensorPin = A0;
 const int motorPins = {1, 2}; //Not on the Arduino, on the shield!
+const int buttonPin = A2; //Start button
 
-const float kp = 1, ki = 0.001, kd = 100;
-
-float linePos, lastPos, integral = 0;
+int sensorValues[3];
+float linePos, lastPos, correction, integral = 0; //Higher values indicate further to the right
 long unsigned int lastTime, loopTime = 0; //0 to indicate first loop (i.e. don't set anything)
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); // Create the motor shield object with the default I2C address
@@ -18,16 +23,42 @@ Adafruit_DCMotor *rightMotor = AFMS.getMotor(motorPin[1]);
 
 void setup() {
 
-  for {int i=0; i<3; i++} pinMode(ledPins[i], OUTPUT);
+  for (int i=0; i<3; i++} pinMode(ledPins[i], OUTPUT);
   pinMode(sensorPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
 
-  AFMS.begin();
+  AFMS.begin(); //Initialize the motor shield
+
+  while (digitalRead(buttonPin) == HIGH) delay(10); //Wait for button press
 
 }
 
 void loop() {
 
-  myMotor->setSpeed(255);
-  myMotor->run(FORWARD);
+  //First, read the sensors
+  for(int i=0; i < numLeds; i++){
+    digitalWrite(ledPins[i], HIGH);
+    delay(readDelay);  
+    sensorValues[i] = analogRead(sensorPin);
+    digitalWrite(ledPins[i], LOW);
+  }
+
+  //Calculate line position based on sensor readings
+  linePos = (sensorValues[numLeds-1] - sensorValues[0]) * sensorDiffToLinePos; //This should be tuned!
+
+  //Do the PID thing
+  integral += linePos;
+  correction = (kp * linePos) + (ki * integral) + (kd * (linePos-lastPos));
+  lastPos = linePos;
+  if (correction > 255) correction = 255; //Clamp to usable values
+  if (correction < -255) correction = -255;
+
+  //Set the motor speeds
+  rightMotor->setSpeed(correction < 0 ? 255 : 255-correction);
+  leftMotor->setSpeed(correction > 0 ? 255 : 255+correction);
+
+  //And tell them to run
+  rightMotor->run(FORWARD);
+  leftMotor->run(FORWARD);
 
 }
