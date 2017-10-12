a37cd8157163c97f0b946c7b5004a9cc5a8a012a
Adam Novotny <adam.novotny@postrock.wall>
2017-10-12 17:25:22 -0400

    Add IRLib

--- labIR/src/main.cpp
+++ labIR/src/main.cpp
@@ -1,4 +1,8 @@
 #include "Arduino.h"
+#include "IRLibAll.h"
+
+IRrecvPCI myReceiver(2);
+IRdecode myDecoder;
 
 int IR_pin = A0;
 int a[3] = {};
@@ -6,20 +10,28 @@ int a[3] = {};
 void setup()
 {
   Serial.begin(9600);
+  myReceiver.enableIRIn();
   Serial.println("Begin");
 
 }
 
 void loop()
 {
-  // a = {0, 0, 0};
-  for(int i = 0; i < 3; i++){
-    a[i] = analogRead(IR_pin);
+
+  if (myReceiver.getResults()) {
+    myDecoder.decode();           //Decode it
+    myDecoder.dumpResults(true);  //Now print results. Use false for less detail
+    myReceiver.enableIRIn();      //Restart receiver
   }
-  for(int i = 0; i < 3; i++){
-    Serial.print(a[i]);
-    Serial.print("\t");
-  }
-  Serial.println();
-  delay(50);
+
+  // // a = {0, 0, 0};
+  // for(int i = 0; i < 3; i++){
+  //   a[i] = analogRead(IR_pin);
+  // }
+  // for(int i = 0; i < 3; i++){
+  //   Serial.print(a[i]);
+  //   Serial.print("\t");
+  // }
+  // Serial.println();
+  // delay(50);
 }
