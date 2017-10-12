f9b577f3f4e765b82b3decd43074f346d65e813f
Adam Selker <aselker@openmailbox.org>
2017-10-12 19:32:27 -0400

    More PID, with a remote.  Now to make the hardware work so we can test...

--- testPID/src/main.cpp
+++ testPID/src/main.cpp
@@ -2,16 +2,24 @@
 
 #include "Arduino.h"
 #include <Adafruit_MotorShield.h>
+#include "IRLibAll.h"
 
 const int readDelay = 10;
 const float sensorDiffToLinePos = 0.1;
-const float kp = 1, ki = 0.001, kd = 100;
+const float kp = 100, ki = 0.001, kd = 100;
+const int turnSpeedLeft = 255, turnSpeedRight = 0;
+
+const int turnLength = 20; //iterations after releasing button to keep turning
+int turnCounter = 0; //How long, in iterations, left in this turn
 
 const int numLeds = 3;
-const int ledPins = {4, 5, 6};
+const int ledPins[] = {3, 4, 5};
 const int sensorPin = A0;
-const int motorPins = {1, 2}; //Not on the Arduino, on the shield!
+const int motorPins[] = {1, 2}; //Not on the Arduino, on the shield!
 const int buttonPin = A2; //Start button
+
+IRrecvPCI myReceiver(2); //IR remote receiver on digital pin 2
+IRdecode myDecoder; //IR remote decoder
 
 int sensorValues[3];
 float linePos, lastPos, correction, integral = 0; //Higher values indicate further to the right
@@ -25,42 +33,59 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Begin");
 
-  for (int i=0; i<3; i++} pinMode(ledPins[i], OUTPUT);
+  for (int i=0; i<3; i++) pinMode(ledPins[i], OUTPUT);
   pinMode(sensorPin, INPUT);
   pinMode(buttonPin, INPUT_PULLUP);
 
   AFMS.begin(); //Initialize the motor shield
+  myReceiver.enableIRIn(); //Initialize the remote receiver
 
-  while (digitalRead(buttonPin) == HIGH) delay(10); //Wait for button press
+  //while (digitalRead(buttonPin) == HIGH) delay(10); //Wait for button press
 
 }
 
 void loop() {
 
-  //First, read the sensors
-  for(int i=0; i < numLeds; i++){
-    digitalWrite(ledPins[i], HIGH);
-    delay(readDelay);
-    sensorValues[i] = analogRead(sensorPin);
-    digitalWrite(ledPins[i], LOW);
-  }
+  if (myReceiver.getResults()) { //First, read from the remote, since that can change lots of behavior
+    myDecoder.decode();           //Decode it
+    //myDecoder.dumpResults(true);  //Now print results -- or don't if we want speed
+    myReceiver.enableIRIn();      //Restart receiver
+    turnCounter = turnLength;
+  } else if (turnCounter > 0) turnCounter--;
 
-  //Calculate line position based on sensor readings
-  linePos = (sensorValues[numLeds-1] - sensorValues[0]) * sensorDiffToLinePos; //This should be tuned!
+  if (turnCounter) { //If we're in a human-triggered sharp turn...
+    leftMotor->setSpeed(turnSpeedLeft);
+    rightMotor->setSpeed(turnSpeedRight);
+  } else { //If we're not in a sharp turn
 
-  //Do the PID thing
-  integral += linePos;
-  correction = (kp * linePos) + (ki * integral) + (kd * (linePos-lastPos));
-  lastPos = linePos;
-  if (correction > 255) correction = 255; //Clamp to usable values
-  if (correction < -255) correction = -255;
+    //Read the sensors
+    for(int i=0; i < numLeds; i++){
+      digitalWrite(ledPins[i], HIGH);
+      delay(readDelay);
+      sensorValues[i] = analogRead(sensorPin);
+      digitalWrite(ledPins[i], LOW);
+    }
 
-  //Set the motor speeds
-  rightMotor->setSpeed(correction < 0 ? 255 : 255-correction);
-  leftMotor->setSpeed(correction > 0 ? 255 : 255+correction);
+    //Calculate line position based on sensor readings
+    linePos = (sensorValues[numLeds-1] - sensorValues[0]) * sensorDiffToLinePos; //This should be tuned!
 
-  //And tell them to run
-  rightMotor->run(FORWARD);
-  leftMotor->run(FORWARD);
+    //Do the PID thing
+    integral += linePos;
+    correction = (kp * linePos) + (ki * integral) + (kd * (linePos-lastPos));
+    Serial.println("P: " + String(kp * linePos) + " I: " + (ki * integral) + " D: " + (kd * (linePos-lastPos)));
+    lastPos = linePos;
+    if (correction > 255) correction = 255; //Clamp to usable values
+    if (correction < -255) correction = -255;
+
+    //Set the motor speeds
+    rightMotor->setSpeed(correction < 0 ? 255 : 255-correction);
+    leftMotor->setSpeed(correction > 0 ? 255 : 255+correction);
+
+    //And tell them to run
+    rightMotor->run(FORWARD);
+    leftMotor->run(FORWARD);
+
+
+  } //end if turning
 
 }
