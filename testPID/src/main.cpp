be85b31156e3b0692a48c5e93915bd7d79e82995
Adam Novotny <adam.novotny@postrock.wall>
2017-10-13 00:12:52 -0400

    Add comments

--- testPID/src/main.cpp
+++ testPID/src/main.cpp
@@ -4,27 +4,29 @@
 #include <Adafruit_MotorShield.h>
 #include "IRLibAll.h"
 
+
 const int readDelay = 10;
 float kp = 0.6, ki = 0.0004, kd = 1.5;
 //Working but slow: 30, 0.01, 50, speed 40, cal 60 1.4
 
+// Settings for speed
 const int fast_speed = 40;
 const int slow_speed = fast_speed; //Spurious triggers -> keep them the same
 int cur_speed = slow_speed;
 bool fast = false;
 
+const bool DEBUG = false;
+// Sensor calibration settings
 const int sensor_calibration_offset = 60;
 const float sensor_calibration_scale = 1.4;
-const int turnSpeedLeft = 255, turnSpeedRight = 0;
 
-const int turnLength = 20; //iterations after releasing button to keep turning
-int turnCounter = 0; //How long, in iterations, left in this turn
-
+// Integral variables
 int integral[50];
 const int integral_window = 20;
 int integral_i = 0;
 int integral_sum = 0;
 
+// Pin setup
 const int numLeds = 2;
 const int ledPins[] = {7, 5};
 const int sensorPin = A0;
@@ -38,6 +40,7 @@ const int spareGround = 3; //Ground pin for great good
 IRrecvPCI myReceiver(remotePin); //IR remote receiver
 IRdecode myDecoder; //IR remote decoder
 
+// Sensor and PID values
 int sensorValues[3];
 float linePos, lastPos, correction = 0; //Higher values indicate further to the right
 long unsigned int lastTime, loopTime = 0; //0 to indicate first loop (i.e. don't set anything)
@@ -49,6 +52,7 @@ Adafruit_DCMotor *rightMotor = AFMS.getMotor(motorPins[1]);
 void setup() {
   Serial.begin(9600);
 
+  // Init pins:
   for (int i=0; i<3; i++) pinMode(ledPins[i], OUTPUT);
   pinMode(sensorPin, INPUT);
   pinMode(buttonPin, INPUT_PULLUP);
@@ -67,7 +71,9 @@ void setup() {
   Serial.println("Begin");
 }
 
+// Function to update parameters over serial
 void serial_tune_pid() {
+  // If recieve any value, enter the edit mode
   if(Serial.available()){
     rightMotor->run(RELEASE);
     leftMotor->run(RELEASE);
@@ -83,11 +89,13 @@ void serial_tune_pid() {
     Serial.println("... Reading");
     Serial.println("cur:" + String(kp) + " " + String(ki) + " " + String(kd) + " " + cur_speed);
 
+    // Keep listening until it receives num_inputs parameters
     while(parameter_i < num_inputs) {
       if(Serial.available()){
         char c = Serial.read();
         Serial.print(c);
 
+        // On space save the values
         if(c == ' '){
           in_string[c_index++] = '\0';
           Serial.println(in_string);
@@ -112,6 +120,7 @@ void serial_tune_pid() {
     }
     Serial.println();
 
+    // Set the variables to new values
     kp = in_values[0];
     ki = in_values[1];
     kd = in_values[2];
@@ -124,6 +133,7 @@ void serial_tune_pid() {
 void loop() {
   serial_tune_pid();
 
+  // Read IR remote
   if (myReceiver.getResults()) { //First, read from the remote, since that can change lots of behavior
     myDecoder.decode();           //Decode it
     // myDecoder.dumpResults(true);  //Now print results -- or don't if we want speed
@@ -135,59 +145,55 @@ void loop() {
     Serial.println("GOTTA GO FAST");
   } // else if (turnCounter > 0) turnCounter--;
 
-  if (turnCounter) { //If we're in a human-triggered sharp turn...
-    leftMotor->setSpeed(turnSpeedLeft);
-    rightMotor->setSpeed(turnSpeedRight);
-  } else { //If we're not in a sharp turn
 
-    //Read the sensors
-    for(int i=0; i < numLeds; i++){
-      digitalWrite(ledPins[i], HIGH);
-      delay(readDelay);
-      sensorValues[i] = analogRead(sensorPin);
-      digitalWrite(ledPins[i], LOW);
+  //Read the sensors
+  for(int i=0; i < numLeds; i++){
+    digitalWrite(ledPins[i], HIGH);
+    delay(readDelay);
+    sensorValues[i] = analogRead(sensorPin);
+    digitalWrite(ledPins[i], LOW);
+  }
+
+  if(DEBUG){
+    for(int i=0; i<numLeds; i++){
+      Serial.print(sensorValues[i]);
+      Serial.print("\t");
     }
-
-    // for(int i=0; i<numLeds; i++){
-    //   Serial.print(sensorValues[i]);
-    //   Serial.print("\t");
-    // }
-    // ADD auto calibration on button press
-    /*Serial.print(sensorValues[0] - sensorValues[1]);
     Serial.print("\t");
-    Serial.print(((sensorValues[0] - sensorValues[1]) + sensor_calibration_offset) * sensor_calibration_scale);*/
+    Serial.print(((sensorValues[0] - sensorValues[1]) + sensor_calibration_offset) * sensor_calibration_scale);
+  }
 
-    //Calculate line position based on sensor readings
-    linePos = ((sensorValues[0] - sensorValues[1]) + sensor_calibration_offset) * sensor_calibration_scale; //This should be tuned!
+  //Calculate line position based on sensor readings
+  linePos = ((sensorValues[0] - sensorValues[1]) + sensor_calibration_offset) * sensor_calibration_scale; //This should be tuned!
 
-    //Do the PID thing
-    integral_i++;
-    if(integral_i > integral_window){ integral_i = 0; }
-    integral[integral_i] = linePos;
+  // Update integral
+  integral_i++;
+  if(integral_i > integral_window){ integral_i = 0; }
+  integral[integral_i] = linePos;
 
-    integral_sum = 0;
-    for(int i=0; i<integral_window; i++){
-      integral_sum += integral[i];
-    }
-
-
-    correction = (kp * linePos) + (ki * integral_sum) + (kd * (linePos-lastPos));
-    //Serial.print("\t" + String(correction));
-    Serial.println("P: " + String(kp * linePos) + " I: " + (ki * integral_sum) + " D: " + (kd * (linePos-lastPos)) + " Cor: " + correction);
-    lastPos = linePos;
-    if (correction > cur_speed) correction = cur_speed; //Clamp to usable values
-    if (correction < -cur_speed) correction = -cur_speed;
-
-    //Set the motor speeds
-    rightMotor->setSpeed(correction < 0 ? cur_speed : cur_speed-correction);
-    leftMotor->setSpeed(correction > 0 ? cur_speed : cur_speed+correction);
-
-    //And tell them to run
-    rightMotor->run(FORWARD);
-    leftMotor->run(FORWARD);
+  integral_sum = 0;
+  for(int i=0; i<integral_window; i++){
+    integral_sum += integral[i];
+  }
 
 
-    Serial.println();
-  } //end if turning
+  //Do the PID thing
+  correction = (kp * linePos) + (ki * integral_sum) + (kd * (linePos-lastPos));
+  //Serial.print("\t" + String(correction));
+  Serial.println("P: " + String(kp * linePos) + " I: " + (ki * integral_sum) + " D: " + (kd * (linePos-lastPos)) + " Cor: " + correction);
+  lastPos = linePos;
+  if (correction > cur_speed) correction = cur_speed; //Clamp to usable values
+  if (correction < -cur_speed) correction = -cur_speed;
+
+  //Set the motor speeds
+  rightMotor->setSpeed(correction < 0 ? cur_speed : cur_speed-correction);
+  leftMotor->setSpeed(correction > 0 ? cur_speed : cur_speed+correction);
+
+  //And tell them to run
+  rightMotor->run(FORWARD);
+  leftMotor->run(FORWARD);
+
+
+  Serial.println();
 
 }
